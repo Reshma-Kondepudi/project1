18f4869d743c6fc6e0f00ee47a058d1bf0644d80
Reshma <reshmasrinivas224@gmail.com>
2024-11-07 09:09:29 +0530

    1st changes

--- frontend.sh
+++ frontend.sh
@@ -2,13 +2,14 @@ dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
 
+cp ngnix.conf /etc/ngnix/ngnic.conf
 
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp ngnix.conf /etc/ngnix/ngnic.conf
+
 
 systemctl restart nginx
 systemctl enable nginx
